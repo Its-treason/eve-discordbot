1a555b69a882af4ced2cff77d11ccaae9268d107
Timon <timon.sch71@gmail.com>
2020-02-19 22:11:32 +0100

    update to Version 1.2

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # eve-discordbot
+
+## Kommands
++ `?ehre` zum abfragen seiner Ehre.
++ `?spenden <@Benutzer> <Betrag>` um jemanden Ehre zu Spenden
++ `?fordern <@Benutzer> <Betrag>` um Ehre von jemanden zu nehmen
++ `?time [@Benutzer]` Um die Zeit abzufragen die man im Sprachchat verbracht hat
++ `?bite <@Benutzer>` Um jemanden zu beißen
++ `?hug <@Benutzer>` Um jemanden zu Ummarmen
++ `?version` Gibt die version aus
+
+## Changelog
+#### 1.2
++ Die Zeit die man im Sprachchat wird gezählt
++ Mit `?time [@Benutzer]` kann man seine bzw die eines Benutzers anbfragen. 
--- src/version.js
+++ src/version.js
@@ -2,10 +2,11 @@ module.exports = {
     version: function version(RichEmbed, message) {
         const embed = new RichEmbed()
             .setColor('#0000FF')
-            .setTitle('EVE Version: 1.1.0')
-            .setDescription('Created by Timon Schünemann')
+            .setTitle('EVE Version: 1.2.0')
+            .setDescription('Programmiert von its-treason')
             .setThumbnail('https://cdn.discordapp.com/app-icons/605426789396774912/c7d8ad657013c2ca5dc8ddf4ee8df7af.png?size=512')
+            .setURL("https://github.com/Its-treason/eve-discordbot");
         message.channel.send(embed);
-        console.log('[ OK ] ' + message.author.username + ' checked version (EVE v1.1.0)');
+        console.log('[ OK ] ' + message.author.username + ' checked version');
     }
 }
